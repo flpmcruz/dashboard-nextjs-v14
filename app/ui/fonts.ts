afbf424b3c27c075909698b54892e2536abf669a
Felipe <flpmireles@gmail.com>
2023-11-03 13:43:37 -0500

    initial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Roboto, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const roboto = Roboto({ subsets: ["greek"], weight: ["400", "700"] });
+export const lusitana = Lusitana({ subsets: ["latin"], weight: ["400"] });
